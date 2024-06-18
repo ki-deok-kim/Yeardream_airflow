1bf49085cc458d82b144cc91dc61b6e918bbd4bc
kdkim <oiioxqqq@gmail.com>
2024-06-18 17:28:22 +0900

    test

--- dags/dags_python_decorator_with_param.py
+++ dags/dags_python_decorator_with_param.py
@@ -4,7 +4,7 @@ from airflow.decorators import task
 
 with DAG(
     dag_id="dags_python_decorator_with_param",
-    schedule="0 2 * * 1",
+    schedule="1 2 * * 1",
     start_date=pendulum.datetime(2024, 6, 14, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
